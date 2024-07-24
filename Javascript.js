1594f78b02a552329f0ea29672b6847365b2a622
Baltharu5 <baltharus@gmail.com>
2024-07-23 19:10:26 -0500

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,48 @@
+function rollDice() {
+    var results = "";
+    var numDice = Number(document.getElementById("diceNum").value);
+    var myArray = []
+    
+  var total = 0
+  var count = 0
+  
+    for (var i = 0; i < numDice; i++) {
+    var diceRoll = Math.floor(Math.random() * 6) + 1;
+    myArray.push(diceRoll)
+    total += diceRoll
+    count++
+
+  }
+    document.getElementById("diceTotal").innerText = total
+  document.getElementById("diceCount").innerText = count
+results = "<table>";
+    for (var i = 0; i < myArray.length; i+=6 )
+    {
+        
+        results += "<tr><td>" + myArray[i] + "  " + ( myArray[i+1]===undefined ? '' : myArray[i+1]) + "  " + ( myArray[i+2]===undefined ? '' : myArray[i+2]) + "  " + ( myArray[i+3]===undefined ? '' : myArray[i+3]) + "  " + ( myArray[i+4]===undefined ? '' : myArray[i+4])+ "  " + ( myArray[i+5]===undefined ? '' : myArray[i+5]) + "</tr></td>";  
+    }
+
+    results += "</table>";
+
+    var div = document.getElementById("associatedAssets");
+    div.innerHTML = results;  
+    
+
+let array = myArray
+  
+let oddNum = 0; 
+let evenNum = 0; 
+  
+for (let index = 0; index < array.length; index++) { 
+    if (array[index] % 2 == 0) { 
+        evenNum++; 
+    } 
+    else { 
+        oddNum++; 
+    } 
+} 
+  
+// Printing the result 
+document.getElementById("evens").innerText = evenNum; 
+document.getElementById("odds").innerText = oddNum;
+}
